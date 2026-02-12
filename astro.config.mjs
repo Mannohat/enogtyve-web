c9a638d1ac386db67b06e4f43303f298d17f9523
Mannohat <slybtc@proton.me>
2026-02-12 13:26:34 +0100

    switch to static build for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,6 @@
 import { defineConfig } from 'astro/config';
 import pagefind from 'astro-pagefind';
 
-import node from '@astrojs/node';
-
 export default defineConfig({
   integrations: [pagefind()],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
