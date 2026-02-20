582b1b9a1600c43bca7466ff26ef535232837153
Mannohat <slybtc@proton.me>
2026-02-20 08:49:25 +0100

    seo and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import pagefind from 'astro-pagefind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [pagefind()],
+  site: 'https://enogtyve.org',
+  integrations: [
+    pagefind(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'da',
+        locales: {
+          da: 'da-DK',
+          en: 'en-US',
+        },
+      },
+    }),
+  ],
 });
